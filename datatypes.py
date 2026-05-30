6fd53cdbec95a5771a6e8929cd474542c3df49eb
vishal-kushwaha-9912 <vishalk.ushwa9912@gmail.com>
2026-05-30 05:51:13 +0530

    first commit

---
+++ datatypes.py
@@ -0,0 +1,26 @@
+#INTEGER: there is no limit to the size of an integer in Python, as long as you have enough memory to store it.
+#the largest integer is 9223372036854775807 and the smallest integer is -9223372036854775808
+
+print(12)
+print(-12)
+
+#FLOAT: it is a number that has a decimal point. It can also be in scientific notation.
+# the largest float is 1.7976931348623157e+308 and the smallest float is 2.2250738585072014e-308
+
+print(12.5)
+print(-12.5)
+
+#BOOLEAN: it is a data type that can have only two values: True or False.
+print(True)
+print(False)
+
+#COMPLEX :  python also has a built-in complex data type that can be used to represent complex numbers. 
+# A complex number is a number that has a real part and an imaginary part. 
+# The real part is represented by a float, and the imaginary part is represented by a float followed by the letter "j".
+print(4+5j)
+
+#STRING: it is a sequence of characters enclosed in single quotes, double quotes, or triple quotes.
+#these all represent the same output after executing the code .
+print('hello world')
+print("hello world")
+print('''hello world''')
